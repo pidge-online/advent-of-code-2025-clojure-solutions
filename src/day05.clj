ece67191b2841f07357c4e04139e9dbf91dac0dd
pidge <pigeon-programmer@tuta.io>
2025-12-05 18:15:12 +0000

    Completed Day 5 part 1 solution, struggling with part 2 for some reason

---
+++ src/day05.clj
@@ -0,0 +1,33 @@
+(ns day05)
+(require '[clojure.string :as str])
+
+(def input-ranges (as-> "resources/puzzle-inputs/day05-ranges.txt" input
+                    (slurp input)
+                    (str/split input #"\n")
+                    (mapv #(str/split % #"-") input)
+                    (mapv #(mapv (fn [x] (Long/parseLong x)) %) input)
+                    (vec (sort input)))) ;; drop if cuases issues during res
+
+(def input-ids (as-> "resources/puzzle-inputs/day05-ids.txt" input
+                 (slurp input)
+                 (str/split input #"\n")
+                 (mapv #(Long/parseLong %) input)
+                 (sort input)))
+
+(defn ids-in-ranges
+  ([ids ranges] (ids-in-ranges ids ranges 0))
+  ([ids ranges acc]
+   (cond
+     (empty? ids) acc
+     (empty? ranges) (recur (rest ids) input-ranges acc)
+     :else (let [[lower-range-bound upper-range-bound] (first ranges)
+                 id                                    (first ids)]
+             (if (and
+                  (> id lower-range-bound)
+                  (< id upper-range-bound))
+               (recur (rest ids) input-ranges (+ acc 1))
+               (recur ids (rest ranges) acc))))))
+
+;; p1 solution 
+(ids-in-ranges input-ids input-ranges)
+
