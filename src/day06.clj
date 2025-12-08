58f6ffb8e5ee32e90ac22b2771cbcf9e891c38bc
pidge <pigeon-programmer@tuta.io>
2025-12-08 17:35:16 +0000

    Day 6 part 1 solution complete

---
+++ src/day06.clj
@@ -0,0 +1,25 @@
+(ns day06)
+(require '[clojure.string :as str])
+
+(defn transpose [data]
+  (apply (partial mapv vector) data))
+
+(defn perfom-math [[args operation]]
+  (apply (resolve (symbol operation)) args))
+
+(defn strings-to-ints-and-symbol
+  ([[a b c d operation]] (strings-to-ints-and-symbol [a b c d operation] identity))
+  ([[a b c d operation] f]
+   (vector (mapv #(Integer/parseInt %) (f [a b c d])) (symbol operation))))
+
+(def input-data (as-> "resources/puzzle-inputs/day06.txt" input
+                  (slurp input)
+                  (str/split input #"\n")
+                  (mapv #(str/split % #"\s+") input)
+                  (transpose input))) ; transpose nested vectors
+
+;; p1 solution
+(->> input-data
+     (mapv #(strings-to-ints-and-symbol %))
+     (mapv #(perfom-math %))
+     (reduce +))
