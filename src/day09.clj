95baa700b4b54f7739f767ad28d47397f1ea3f35
Pidge <pigeon-programmer@tuta.io>
2025-12-12 12:44:31 +0000

    Completed Day 9 solution using geometric intuition

--- src/day09.clj
+++ src/day09.clj
@@ -1,61 +1,26 @@
-(ns day09)
-(require '[clojure.string :as str])
-(require '[clojure.math :as math])
-
-
-(def input-data (as-> "resources/puzzle-inputs/day09.txt" input
-                  (slurp input)
-                  (str/split input #"\n")
-                  (mapv #(vec (str/split % #",")) input)
-                  (mapv #(mapv (fn [x] (Integer/parseInt x)) %) input)))
+(ns day09
+  (:require '[clojure.string :as str]))
 
 (defn calculate-area [[x y] [x' y']]
   (abs (*
         (+ (abs (- y' y)) 1)
         (+ (abs (- x' x)) 1))))
 
-(defn compare-areas [input-tiles]
-  (loop [first-tile (first input-tiles)
-         tile-number 1
-         current-max-area (calculate-area first-tile (nth input-tiles 1))]
-    (if (= (inc tile-number) (count input-tiles))
-      current-max-area
-      (let [next-tile-number (inc tile-number)
-            new-area (calculate-area first-tile (nth input-tiles next-tile-number))]
-        (if (> new-area current-max-area)
-          (recur first-tile next-tile-number new-area)
-          (recur first-tile next-tile-number current-max-area))))))
-
-(defn iterate-through-tiles [input-tiles]
-  (let [inital-max (compare-areas input-tiles)
-        total-tiles (count input-tiles)]
-    (loop [n 0
-           max-area inital-max]
-      (if (= (+ n 2) total-tiles)
-        max-area
-        (let [new-area (compare-areas (subvec input-tiles (inc n) total-tiles))
-              new-max-area (max max-area new-area)]
-          (recur (inc n) new-max-area))))))
-
-; p1 solution
-(iterate-through-tiles input-data)
-
-(defn generate-co-ord-comparisons [[x y] [x' y']]
+(defn generate-co-ord-comparisons [[x y] [x' y']] ; producing reversed comparison pairs for determining presence of other validating grid values
   (let [xprime-leading-comparison [(if (> x' x)
-                                     >=
-                                     <=)
+                                     [>= <=]
+                                     [<= >=])
                                    (if (> y y')
-                                     >=
-                                     <=)]
-        yprime-leading-comparison (reverse xprime-leading-comparison)]
-    [xprime-leading-comparison yprime-leading-comparison]))
+                                     [>= <=]
+                                     [<= >=])]]
+    [[(get (first xprime-leading-comparison) 0) (get (last xprime-leading-comparison) 0)] 
+     [(get (first xprime-leading-comparison) 1) (get (last xprime-leading-comparison) 1)]]))
 
 (defn produce-in-bounds-status-list-per-co-ord [input-tiles [x y] [x' y']]
   (let [[[x'-dir y-dir] [x-dir y'-dir]] (generate-co-ord-comparisons [x y] [x' y'])]
     (vec (for [r (range 0 (count input-tiles))]
            (let [[a b] (get input-tiles r)]
-             [[a b]
-              (if (and
+             [(if (and
                    (x'-dir a x')
                    (y-dir b y))
                 true
@@ -66,57 +31,87 @@
                 true
                 false)])))))
 
-(defn verify-in-bounds-co-ord? [nested-status-list]
+(defn verify-bounded-by-other-co-ords? [nested-status-list]
   (and (not (nil? (some #{[false true]} nested-status-list)))
        (not (nil? (some #{[true false]} nested-status-list)))))
 
-(defn compare-areas-in-bounds [input-tiles original-tiles]
-  (loop [[x y] (first input-tiles)
-         tile-number 1
-         current-max-area (calculate-area [x y] (nth input-tiles 1))]
-    (if (= (inc tile-number) (count input-tiles))
-      current-max-area
-      (let [next-tile-number (inc tile-number)
-            [x' y'] (nth input-tiles next-tile-number)
-            new-area (if (verify-in-bounds-co-ord? (produce-in-bounds-status-list-per-co-ord original-tiles [x y] [x' y']))
-                       (calculate-area [x y] [x' y'])
-                       0)]
-        (if (> new-area current-max-area)
-          (recur [x y] next-tile-number new-area)
-          (recur [x y] next-tile-number current-max-area))))))
+(defn invalid-by-concave-path-passthrough? [[x y] [x' y'] [[a b] [a' b']]]
+  (let [lx (max x x') ; l - largest
+        sx (min x x') ; s - smallest
+        ly (max y y')
+        sy (min y y')
+        la (max a a')
+        sa (min a a')
+        lb (max b b')
+        sb (min b b')]
+    (if (or
+         (and (> lx la sx) (> lx sa sx) (or (> lb sy sb) (> lb ly sb))) ; does the pair of points cross through a horizontal rectangle bound
+         (and (> ly lb sy) (> ly sb sy) (or (> la sx sa) (> la lx sa))))  ; does the pair of points cross through a vertical rectangle bound
+      true
+      false)))
 
-(defn iterate-through-in-bounds-tiles [input-tiles]
-  (let [inital-max (compare-areas-in-bounds input-tiles input-tiles)
+(defn verify-coordinates-not-passed-through? [[x y] [x' y'] step-pathing]
+  (loop [steps step-pathing
+         out-of-bounds-detected? (invalid-by-concave-path-passthrough? [x y] [x' y'] (first steps))]
+    (cond
+      out-of-bounds-detected?      false 
+      (= 1 (count steps))          true
+      :else                        (let [remaining-steps (rest steps)
+                                         next-out-of-bounds? (invalid-by-concave-path-passthrough? [x y] [x' y'] (first remaining-steps))]
+                                     (recur remaining-steps next-out-of-bounds?)))))
+
+(defn verify-and-calculate-new-area [original-tiles [x y] [x' y'] step-pathing]
+  (if (and 
+       (verify-bounded-by-other-co-ords? (produce-in-bounds-status-list-per-co-ord original-tiles [x y] [x' y']))
+       (verify-coordinates-not-passed-through? [x y] [x' y'] step-pathing)
+       true)
+    (calculate-area [x y] [x' y'])
+    0))
+
+(defn compare-areas
+  ([input-tiles] (compare-areas input-tiles false false))
+  ([input-tiles original-tiles step-pathing]
+   (loop [[x y] (first input-tiles)
+          tile-number 1
+          current-max-area (calculate-area [x y] (nth input-tiles 1))]
+     (if (= (inc tile-number) (count input-tiles))
+       current-max-area
+       (let [next-tile-number (inc tile-number)
+             [x' y']          (nth input-tiles next-tile-number)
+             new-area         (if step-pathing
+                                (verify-and-calculate-new-area original-tiles [x y] [x' y'] step-pathing)
+                                (calculate-area [x y] (nth input-tiles next-tile-number)))]
+         (if (> new-area current-max-area)
+           (recur [x y] next-tile-number new-area)
+           (recur [x y] next-tile-number current-max-area)))))))
+
+(defn iterate-through-tiles ([input-tiles] (iterate-through-tiles input-tiles false)) 
+  ([input-tiles step-pathing]
+  (let [inital-max (if step-pathing
+                     (compare-areas input-tiles input-tiles step-pathing)
+                     (compare-areas input-tiles))
         total-tiles (count input-tiles)]
     (loop [n 0
            max-area inital-max]
       (if (= (+ n 2) total-tiles)
         max-area
-        (let [new-area (compare-areas-in-bounds (subvec input-tiles (inc n) total-tiles) input-tiles)
+        (let [new-area (if step-pathing
+                         (compare-areas (subvec input-tiles (inc n) total-tiles) input-tiles step-pathing)
+                         (compare-areas (subvec input-tiles (inc n) total-tiles)))
               new-max-area (max max-area new-area)]
-          (recur (inc n) new-max-area))))))
+          (recur (inc n) new-max-area)))))))
 
+(def input-data (->> "resources/puzzle-inputs/day09.txt"
+                  (slurp)
+                  (str/split-lines)
+                  (mapv #(vec (str/split % #",")))
+                  (mapv #(mapv (fn [x] (Integer/parseInt x)) %))))
+
+; p1 solution
+(iterate-through-tiles input-data)
 
 ; p2 solution
-(iterate-through-in-bounds-tiles input-data)
+(def tile-pathing-steps (vec (map vec (partition 2 1 input-data))))
+(iterate-through-tiles input-data tile-pathing-steps)
 
-(def sample-data (as-> "7,1
-11,1
-11,7
-9,7
-9,5
-2,5
-2,3
-7,3" input
-                   (str/split input #"\n")
-                   (mapv #(vec (str/split % #",")) input)
-                   (mapv #(mapv (fn [x] (Integer/parseInt x)) %) input)))
-
-(assert (= 24 (iterate-through-in-bounds-tiles sample-data))) ; nil means assertion is true, otherwise throws assertion error
-
-;; current too large value co-ords [95990 63461] [4061 37808]
-;; ([[3836 64177] true false])
-;; ([[96087 37113] false true])
-;; not accounted for concave nature of polygon
-;; need to find maximum spanning rectangle area inside concave polygon algorithm
 
