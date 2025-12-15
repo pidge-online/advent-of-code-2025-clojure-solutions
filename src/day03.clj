452e927597043ed8ed001587c3a5e29b37766854
Pidge <pigeon-programmer@tuta.io>
2025-12-15 15:05:25 +0000

    Refactor input preparation and main function

--- src/day03.clj
+++ src/day03.clj
@@ -1,33 +1,33 @@
 (ns day03
-  (:require 
+  (:require
    [clojure.string :as str]))
 
 (def input-prep (as-> "resources/puzzle-inputs/day03.txt" input
-                   (slurp input)
-                   (str/split input #"\n")
-                   (map #(str/split % #"") input)))
+                  (slurp input)
+                  (str/split input #"\n")
+                  (map #(str/split % #"") input)))
 
 (defn two-battery-joltage [input]
   (let [sorted-batteries (reverse (sort input))
-                largest-joltage (first sorted-batteries)]
-            (if (apply distinct? (take 2 sorted-batteries))
-              (let [substring-to-validate (str/split (str/join "" input) (re-pattern largest-joltage))
-                    other-digit                      (first (reverse (sort (last substring-to-validate))))]
-                (if (= (count substring-to-validate) 1)
-                  (Integer/parseInt (str
-                                     other-digit
-                                     largest-joltage))
-                  (Integer/parseInt (str
-                                     largest-joltage
-                                     other-digit))))
-              (* 11 (Integer/parseInt largest-joltage)))))
+        largest-joltage (first sorted-batteries)]
+    (if (apply distinct? (take 2 sorted-batteries))
+      (let [substring-to-validate (str/split (str/join "" input) (re-pattern largest-joltage))
+            other-digit                      (first (reverse (sort (last substring-to-validate))))]
+        (if (= (count substring-to-validate) 1)
+          (Integer/parseInt (str
+                             other-digit
+                             largest-joltage))
+          (Integer/parseInt (str
+                             largest-joltage
+                             other-digit))))
+      (* 11 (Integer/parseInt largest-joltage)))))
 
 (def part1 (as-> input-prep input
-  (map two-battery-joltage input)
-  (apply + input))
+             (map two-battery-joltage input)
+             (apply + input)))
 
 (defn -main []
   (println part1)
-  (println "part 2 TBD")
+  (println "part 2 TBD"))
 
 (-main)
