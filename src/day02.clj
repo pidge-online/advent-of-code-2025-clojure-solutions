7089619cb7a0a6fad6d9a37e27d69995175b14d6
Pidge <pigeon-programmer@tuta.io>
2025-12-15 15:08:31 +0000

    Refactor main function to use part1 and part2

--- src/day02.clj
+++ src/day02.clj
@@ -20,8 +20,12 @@
                   (map #(mapv (fn [x] (Long/parseLong x)) %) input)
                   (map (fn [[a b]] (range a (inc b))) input)))
 
+(def part1 (calculate-total input-prep (repeating-subsequence-compsed-string? "^(.+)\\1$")))
+
+(def part2 (calculate-total input-prep (repeating-subsequence-compsed-string? "^(.+)\\1+$")))
+
 (defn -main []
-  (println (calculate-total input-prep (repeating-subsequence-compsed-string? "^(.+)\\1$")))
-  (println (calculate-total input-prep (repeating-subsequence-compsed-string? "^(.+)\\1+$"))))
+  (println part1)
+  (println part2))
 
 (-main)
