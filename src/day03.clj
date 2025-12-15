d8f56c9c799d7a5664b03652699b425131d33cc4
Pidge <pigeon-programmer@tuta.io>
2025-12-15 17:21:45 +0000

    Day 3 part 2 solution completed
    I had the logic, just didn't realise from the examples how I had to recur across. Much easier and quicker then I first made it, big reminder to look at the data for behaviour for idiosyncrasies and think of edge cases

--- src/day03.clj
+++ src/day03.clj
@@ -2,31 +2,38 @@
   (:require
    [clojure.string :as str]))
 
-(def input-prep (as-> "resources/puzzle-inputs/day03.txt" input
-                  (slurp input)
-                  (str/split input #"\n")
-                  (map #(str/split % #"") input)))
+(def input-data (->> (slurp "resources/puzzle-inputs/day03.txt")
+                  (str/split-lines)
+                  (map #(str/split % #""))))
 
-(defn two-battery-joltage [input]
-  (let [sorted-batteries (reverse (sort input))
-        largest-joltage (first sorted-batteries)]
-    (if (apply distinct? (take 2 sorted-batteries))
-      (let [substring-to-validate (str/split (str/join "" input) (re-pattern largest-joltage))
-            other-digit                      (first (reverse (sort (last substring-to-validate))))]
-        (if (= (count substring-to-validate) 1)
-          (Integer/parseInt (str
-                             other-digit
-                             largest-joltage))
-          (Integer/parseInt (str
-                             largest-joltage
-                             other-digit))))
-      (* 11 (Integer/parseInt largest-joltage)))))
+(defn get-joltage-for-n-digits [n input-data]
+  (loop [remaining-digits          n
+         string                    input-data
+         current-joltage           ""
+         sorted-int-set            (apply sorted-set-by #(compare %2 %1) string)]
+    (if (= 0 remaining-digits)
+      (Long/parseLong current-joltage)
+      (let [largest-int               (str (first sorted-int-set))
+            remaining-ints-post-split (second (str/split (str/join "" string) (re-pattern largest-int) 2))]
+        (if (>= (count remaining-ints-post-split) (dec remaining-digits))
+          (recur
+           (dec remaining-digits)
+           (str/split  remaining-ints-post-split #"")
+           (str current-joltage largest-int)
+           (apply sorted-set-by #(compare %2 %1) remaining-ints-post-split))
+          (recur
+           remaining-digits
+           string
+           current-joltage
+           (rest sorted-int-set)))))))
 
-(def part1 (as-> input-prep input
-             (map two-battery-joltage input)
-             (apply + input)))
+(defn sum-total-joltages [n] (->> input-data
+                                  (map #(get-joltage-for-n-digits n %))
+                                  (apply +)))
 
-(def part2 "part 2 TBD")
+(def part1 (sum-total-joltages 2))
+
+(def part2 (sum-total-joltages 12))
 
 (defn -main []
   (println part1)
