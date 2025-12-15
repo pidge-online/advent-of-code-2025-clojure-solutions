9c97f34d632720cde07af7f050c3bae6259f719b
Pidge <pigeon-programmer@tuta.io>
2025-12-15 18:27:13 +0000

    Completed Day05 part 2

--- src/day05.clj
+++ src/day05.clj
@@ -1,5 +1,5 @@
 (ns day05
-  (:require 
+  (:require
    [clojure.string :as str]))
 
 (def input-ranges (as-> "resources/puzzle-inputs/day05-ranges.txt" input
@@ -29,9 +29,22 @@
                (recur (rest ids) input-ranges (+ acc 1))
                (recur ids (rest ranges) acc))))))
 
+(defn adjust-range-bounds-for-overlap [ranges]
+  (loop [remaining-ranges (rest ranges)
+         [cur-start cur-end] (first ranges)
+         merged-ranges []]
+    (if (empty? remaining-ranges)
+      (conj merged-ranges [cur-start cur-end])
+      (let [[start end] (first remaining-ranges)]
+        (if (<= start (inc cur-end))
+          (recur (rest remaining-ranges) [cur-start (max cur-end end)] merged-ranges)
+          (recur (rest remaining-ranges) [start end] (conj merged-ranges [cur-start cur-end])))))))
+
 (def part1 (ids-in-ranges input-ids input-ranges))
 
-(def part2 "part 2 TBD")
+(def part2 (->> (adjust-range-bounds-for-overlap input-ranges)
+                (mapv (fn [[a b]] (+ (- b a) 1)))
+                (reduce +)))
 
 (defn -main []
   (println part1)
