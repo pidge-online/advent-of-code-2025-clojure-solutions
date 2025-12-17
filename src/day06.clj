9220dae499c5407c59d3dd62ceea63daf2363e09
Pidge <pigeon-programmer@tuta.io>
2025-12-17 18:23:26 +0000

    Completed Day 6 part 2

--- src/day06.clj
+++ src/day06.clj
@@ -1,30 +1,62 @@
 (ns day06
-  (:require 
-   [clojure.string :as str]))
+   (:require
+    [clojure.string :as str]
+    [clojure.walk :as w]))
 
 (defn transpose [data]
   (apply (partial mapv vector) data))
 
-(defn perfom-math [[args operation]]
+(defn sum-from-symbol-and-ints [[operation args]]
   (apply (resolve (symbol operation)) args))
 
-(defn strings-to-ints-and-symbol
-  ([[a b c d operation]] (strings-to-ints-and-symbol [a b c d operation] identity))
-  ([[a b c d operation] f]
-   (vector (mapv #(Integer/parseInt %) (f [a b c d])) (symbol operation))))
+(defn zeroed-cephalid-string-vec-to-standard-int-vec [v]
+  (mapv #(apply str %) (vec (for [x (range 0 (count (first v)))]
+                              (vec (for [y (range 0 (count v))]
+                                     (get-in v [y x])))))))
 
-(def input-data (as-> "resources/puzzle-inputs/day06.txt" input
-                  (slurp input)
-                  (str/split input #"\n")
-                  (mapv #(str/split % #"\s+") input)
-                  (transpose input))) ; transpose nested vectors
+(defn extract-exact-length-digit-inputs [s length-counts]
+  (loop [parsed       []
+         unparsed     s
+         split-counts length-counts]
+    (if (empty? split-counts)
+      parsed
+      (let [[next-int remainder-to-parse] (mapv #(apply str %) (split-at (first split-counts) unparsed))]
+        (recur (conj parsed next-int) (rest remainder-to-parse) (rest split-counts))))))
 
-(def part1 (->> input-data
-                (mapv #(strings-to-ints-and-symbol %))
-                (mapv #(perfom-math %))
-                (reduce +)))
+(defn attach-operations-to-string-list-then-sum-operation-results [operations v] (->> v
+                                                                           (w/postwalk #(if (string? %) (Integer/parseInt %) %))
+                                                                           (mapv #(conj [%1] %2) operations)
+                                                                           (mapv #(sum-from-symbol-and-ints %))
+                                                                           (reduce +)))
 
-(def part2 "part 2 TBD")
+(defn extract-int-rows [v] (->> (take 4 v)
+                                (transpose)))
+
+(def raw-single-string-digit-vector (-> (slurp "resources/puzzle-inputs/day06.txt")
+                                        (str/split #"\n")))
+
+(def input-data (->> raw-single-string-digit-vector
+                     (mapv #(str/split % #"\s+"))
+                     (w/postwalk #(if (vector? %) (vec (remove empty? %)) %))))
+
+(def operation-list (->> (last input-data)
+                         (transpose)
+                         (first)
+                         (mapv #(str %))))
+
+(def largest-digit-length-each-sum (->> (take 4 input-data)
+                                        (transpose)
+                                        (mapv #(apply max (mapv (fn [s] (count s)) %)))))
+
+(def part1 (->> (extract-int-rows input-data)
+                (attach-operations-to-string-list-then-sum-operation-results operation-list)))
+
+(def part2 (->> raw-single-string-digit-vector
+                (mapv #(extract-exact-length-digit-inputs % largest-digit-length-each-sum))
+                (extract-int-rows)
+                (mapv #(zeroed-cephalid-string-vec-to-standard-int-vec %))
+                (mapv #(mapv (fn [s] (str/replace s #"\s" "")) %))
+                (attach-operations-to-string-list-then-sum-operation-results operation-list)))
 
 (defn -main []
   (println part1)
